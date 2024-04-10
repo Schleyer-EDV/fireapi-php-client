870aae92e0f471be3a3404a4095418200738c79a
Björn Schleyer <mail@bschleyer.de>
2024-04-11 00:26:11 +0200

    Implement sandbox

--- src/Credentials.php
+++ src/Credentials.php
@@ -46,7 +46,7 @@ class Credentials
                 break;
             case true:
                 $this->sandbox = true;
-                $this->url = '';
+                $this->url = 'https://sandbox.fireapi.de/';
                 break;
             default:
                 $this->sandbox = false;
